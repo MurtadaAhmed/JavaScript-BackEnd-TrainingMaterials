16ea50c19623d8801db3537231e0b5ead3ef3818
Murtada Ahmed <murtadhabg@gmail.com>
2024-02-25 01:12:52 +0200

    added home page

--- Workshop ExpressJS and Templating/Movie-Magic-Workshop/index.js
+++ Workshop ExpressJS and Templating/Movie-Magic-Workshop/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const handlebars = require('express-handlebars');
+const path = require('path');
+
 const port = 80;
 
+// Set the view engine to handlebars
+app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
+app.set('view engine', 'hbs');
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render("home", {layout: false});
 })
+
+// setting the folder where the static files are located
+app.use(express.static('public'))
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 })
