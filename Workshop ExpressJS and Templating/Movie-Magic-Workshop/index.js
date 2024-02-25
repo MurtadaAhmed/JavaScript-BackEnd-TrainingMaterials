273dcdc0cf2342db7bfae0771cd1e97ad69ca5be
Murtada Ahmed <murtadhabg@gmail.com>
2024-02-25 14:19:46 +0200

    add body parser

--- Workshop ExpressJS and Templating/Movie-Magic-Workshop/index.js
+++ Workshop ExpressJS and Templating/Movie-Magic-Workshop/index.js
@@ -9,6 +9,7 @@ const port = 80;
 // setting the folder where the static files are located
 // it has to be in order before app.use(routes) so the static files are served first before the 404 page
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false })); // this is to extract the data from the form (Create Post)
 
 app.use(routes)
 
