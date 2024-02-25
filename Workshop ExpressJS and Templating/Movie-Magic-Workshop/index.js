b1d97e0b263ae1ed46b6688fff751992eb9fcc5a
Murtada Ahmed <murtadhabg@gmail.com>
2024-02-25 12:20:19 +0200

    added routes and controllers

--- Workshop ExpressJS and Templating/Movie-Magic-Workshop/index.js
+++ Workshop ExpressJS and Templating/Movie-Magic-Workshop/index.js
@@ -2,16 +2,16 @@ const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
 const path = require('path');
-
+const routes = require('./routes');
 const port = 80;
+
+app.use(routes)
 
 // Set the view engine to handlebars
 app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
 app.set('view engine', 'hbs');
 
-app.get('/', (req, res) => {
-    res.render("home", {layout: false});
-})
+
 
 // setting the folder where the static files are located
 app.use(express.static('public'))
