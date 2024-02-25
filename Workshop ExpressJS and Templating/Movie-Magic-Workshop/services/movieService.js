c932f58bea56cad37b2bb54ad245b2058f5b13d8
Murtada Ahmed <murtadhabg@gmail.com>
2024-02-25 14:42:55 +0200

    add a movie

--- Workshop ExpressJS and Templating/Movie-Magic-Workshop/services/movieService.js
+++ Workshop ExpressJS and Templating/Movie-Magic-Workshop/services/movieService.js
@@ -1,5 +1,14 @@
-const movies = []
+const movies = [{
+    title: 'Test title',
+    genre: 'Test Genre',
+    director: 'Test Director',
+    date: '2004',
+    imageURL: 'static\img\jungle-cruise.jpeg',
+    rating: '5',
+    description: 'Test Description'
+  }]
 
 exports.create = (movieData) => {
     movies.push(movieData);
+    console.log(movies);
 }
