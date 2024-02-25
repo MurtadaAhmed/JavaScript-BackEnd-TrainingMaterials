c029fd141dd51b695fa39336d3d8c65f1508b609
Murtada Ahmed <murtadhabg@gmail.com>
2024-02-25 12:41:08 +0200

    add create movie page

--- Workshop ExpressJS and Templating/Movie-Magic-Workshop/routes.js
+++ Workshop ExpressJS and Templating/Movie-Magic-Workshop/routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const HomeController = require('./controllers/HomeController');
- 
-router.use(HomeController);
+const MovieController = require('./controllers/movieController');
+
+router.use(HomeController, MovieController);
 
 module.exports = router;
